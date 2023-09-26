ae8452805e56d538d919374b24e8559a8c89461f
pruedencesatria21 <pruedencesatria@gmail.com>
2023-09-26 20:18:54 +0700

    menambahkan ci dan contoh1

---
+++ application/controllers/contoh.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Pruedence Tri Satria
+              saya tinggal di daerah Bekasi
+              olahraga saya bermain game mobile";
+    }
+}
